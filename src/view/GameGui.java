e0e251761be32e6ab35d351de829d8faf7295d14
awkwardss <chunfengdouruaimi@gmail.com>
2017-04-20 01:52:41 -0400

    modified gamegui.java

--- src/view/GameGui.java
+++ src/view/GameGui.java
@@ -5,56 +5,86 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class GameGui implements ActionListener{
-	//codes for first view: choose difficulty
+public class GameGui{
+		private JFrame frame;
+		private JPanel startButtonPanel;
+		private JButton startButton;
+		private JPanel buttongroupPanel;
+		private JLabel diffLabel;
+		private ButtonGroup group;
+		int gameDifficulty;
+		
+	
+	/*
+	 * First UI class: choose game difficulty and start button
+	 */
 	public void initGUI1(){
 		//better look for frame
 		JFrame.setDefaultLookAndFeelDecorated(true);
+		
 		//frame, level list and start button
-		JFrame startFrame = new JFrame();
-	
-		startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit
-		startFrame.setTitle("Tic-toc-game");
-		startFrame.setSize(500,500);
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit
+		frame.setTitle("Tic-toc-game");
+		frame.setSize(500,500);
 			
 		//difficulty choose box AND start button: 2 panels
-		//start button panel
-		JPanel startButtonPanel = new JPanel();
-		JButton startButton = new JButton("Start!");
-		startButtonPanel.add(startButton);
 		//use JPannel to store buttons
-		JPanel buttongroupPanel = new JPanel();
+		buttongroupPanel = new JPanel();
 		buttongroupPanel.setLayout(new GridLayout(4, 1));//use grid layout for button list
 		//buttongroupPanel.setLayout(new BoxLayout(buttongroupPanel, BoxLayout.Y_AXIS));
-		JLabel diffLabel = new JLabel("Please choose difficult level:");
-		//single choice
-			ButtonGroup group = new ButtonGroup();
-			JRadioButton easyButton = new JRadioButton("Easy", false);
-			JRadioButton midiumButton = new JRadioButton("Intermediate", false);
-			JRadioButton difficultButton = new JRadioButton("Difficult", true);
-			group.add(easyButton);
-			group.add(midiumButton);
-			group.add(difficultButton);	
+		diffLabel = new JLabel("Please choose difficult level:");
 		buttongroupPanel.add(diffLabel);
-		buttongroupPanel.add(easyButton);
-		buttongroupPanel.add(midiumButton);
-		buttongroupPanel.add(difficultButton);
-		
-		startButton.addActionListener(this);
+		//radio button for difficulty
+		group = new ButtonGroup();
+		addRadioButton("Easy", 1);
+		addRadioButton("Intermediate", 2);
+		addRadioButton("Difficult", 3);
+
+		//start button panel
+		startButtonPanel = new JPanel();
+		startButton = new JButton("Start!");
+		startButtonPanel.add(startButton);
+		//listener
+		startButton.addActionListener(new StartButtonListener());
 				
-				
-			
-		startFrame.getContentPane().add(BorderLayout.SOUTH, startButtonPanel);
-		startFrame.getContentPane().add(BorderLayout.CENTER, buttongroupPanel);
+		frame.getContentPane().add(BorderLayout.SOUTH, startButtonPanel);
+		frame.getContentPane().add(BorderLayout.CENTER, buttongroupPanel);
 				
 					
-		startFrame.setVisible(true); 
+		frame.setVisible(true); 
 	}
+	
+	/*
+	 * Method for the GUI1. Add radio button into panel
+	 * @param name: the name of the radio button
+	 * @param difficulty: the difficulty of the game 1-easy 2-medium 3-hard
+	 */
+	public void addRadioButton(String name, int difficulty){
+		JRadioButton button = new JRadioButton(name, true);
+		group.add(button);
+		buttongroupPanel.add(button);
+		
+		ActionListener listener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				gameDifficulty = difficulty;
+				
+			}
+		};
+		
+		button.addActionListener(listener);
+	}
+	
 
-	public void actionPerformed(ActionEvent e) {//action when click start
-		// TODO Auto-generated method stub
-		// get the difficulty level and show game view
-			
+	/*
+	 * inner class for start button listener
+	 */
+	class StartButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event){
+			//frame.repaint();//can call new GUI?
+		}
 	}
 	
 }
