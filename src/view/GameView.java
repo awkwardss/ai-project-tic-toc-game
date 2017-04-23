f78c8210d269254a435949ddbb351c22cc283fec
awkwardss <chunfengdouruaimi@gmail.com>
2017-04-23 00:48:43 -0400

    chessboard

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,7 +1,11 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameView {
 
@@ -33,10 +37,14 @@ public class GameView {
 
 
 		//main game panel AND game info panel
+		//game board panel
 		gamePanel = new JPanel();
-		addChessBoard();
+		//gamePanel.add(new boardComponent());
+		addBoard();
+		gamePanel.setBackground(Color.pink);
 
 
+		// game info panel
 		gameInfoPanel = new JPanel();
 		gameInfoPanel.setLayout(new GridLayout(6, 1));
 
@@ -63,8 +71,50 @@ public class GameView {
 		frame.setVisible(true);
 	}
 
-	private void addChessBoard(){
+	/*
+	class BoardPanel extends JPanel{
+		private Image board;
 
+		public boardComponent(){
+			board = new ImageIcon("img/board.png").getImage();
+		}
+
+		public void paintComponent(Graphics g){
+			if (board != null){
+				int imageHeight = board.getHeight(this);
+				int imageWidth = board.getWidth(this);
+
+				g.drawImage(board, 250, 50, null); //draw board in (200, 50)
+			} else {
+				System.out.print("Fail to read board image!");
+			}
+		}
+	}
+	*/
+	public void addBoard(){
+		//JLabel way: not used
+		//JLabel boardLabel = new JLabel(new ImageIcon("img/board.png"));
+		//gamePanel.add(BorderLayout.CENTER, boardLabel);
+
+		//JPanel way:
+
+	}
+
+	public class BoardPanel extends JPanel {
+		private BufferedImage board;
+
+		public BoardPanel(){
+			try {
+				board = ImageIO.read(new File("img/board.png"));
+			} catch (IOException ex) {
+				System.out.print("Fail to read board image!");
+			}
+		}
+
+		@Override
+		public void paintComponent (Graphics g) {
+
+		}
 	}
 	
 }
